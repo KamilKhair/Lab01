d0ddd6af431c394bdde11da80dc2a7efb33659bd
KamilKhair <KamilK@code-college.net>
2016-06-17 19:11:37 +0300

    Introduction to .NET and C#

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, C#!");
+            Console.WriteLine("My name is Kamil Khair!");
+            Console.WriteLine("I'm from Maghar, Israel!");
+        }
+    }
+}
